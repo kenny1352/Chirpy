38c73228d6f8246f995df41835d4acddf6b2dbfa
hmorgan8 <hmorgan@mail.umw.edu>
2015-11-13 11:40:34 -0500

    Added HomeGUI.java

--- src/Message.java
+++ src/Message.java
@@ -12,6 +12,32 @@ public class Message {
         timestamp = tempT;
         content = tempC;
         publicSetting = tempP;
+
+        //pull hashtags from content
+        boolean reading=false;
+        String topicstring="";
+        for(int i=0; i<content.length(); i++) {
+            if (reading==true) {
+                //log this character into memory
+                topicstring=topicstring+content.charAt(i);
+            }
+            if (content.charAt(i)=='#') {
+                topicstring="";
+                reading=true;
+            }
+            if (content.charAt(i)==' ') {
+                reading=false;
+                hashtag.add(topicstring);
+            }
+        }
+
+        //also somehow give it IDcode based on database
+    }
+
+    public String toString(){
+        String chirpstring;
+        
+        return chirpstring;
     }
 
     private User author;
