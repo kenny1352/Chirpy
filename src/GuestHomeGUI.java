715ae8a7374c024f8f65770e3c11fc2690498957
Catherine <blondiecat14@gmail.com>
2015-11-20 11:10:40 -0500

    GuestHomeGUI change

--- src/GuestHomeGUI.java
+++ src/GuestHomeGUI.java
@@ -67,14 +67,16 @@ public class GuestHomeGUI extends JFrame {
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         // Generated using JFormDesigner Evaluation license - Catherine Merz
-        panel3 = new JPanel();
         title = new JLabel();
-        panel4 = new JSplitPane();
+        panel4 = new JPanel();
+        panel2 = new JPanel();
         panel1 = new JPanel();
         newsfeedButton = new JButton();
+        logoutButton = new JButton();
+        panel5 = new JPanel();
+        textField1 = new JTextField();
         profileSearchButton = new JButton();
         topicSearchButton = new JButton();
-        logoutButton = new JButton();
         scrollPane1 = new JScrollPane();
         textArea1 = new JTextArea();
 
@@ -87,39 +89,39 @@ public class GuestHomeGUI extends JFrame {
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
 
-        //======== panel3 ========
+        //---- title ----
+        title.setText("Guest Newsfeed");
+        title.setFont(new Font("Segoe UI", Font.BOLD, 22));
+        title.setOpaque(true);
+        title.setPreferredSize(new Dimension(42, 50));
+        title.setHorizontalTextPosition(SwingConstants.CENTER);
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setBackground(new Color(204, 255, 255));
+        contentPane.add(title, BorderLayout.PAGE_START);
+
+        //======== panel4 ========
         {
+            panel4.setBackground(new Color(102, 255, 204));
 
             // JFormDesigner evaluation mark
-            panel3.setBorder(new javax.swing.border.CompoundBorder(
+            panel4.setBorder(new javax.swing.border.CompoundBorder(
                 new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
                     "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
                     javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-                    java.awt.Color.red), panel3.getBorder())); panel3.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+                    java.awt.Color.red), panel4.getBorder())); panel4.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
 
-            panel3.setLayout(new BorderLayout());
+            panel4.setLayout(new BorderLayout());
 
-            //---- title ----
-            title.setText("Guest Newsfeed");
-            title.setFont(new Font("Segoe UI", Font.BOLD, 22));
-            title.setOpaque(true);
-            title.setPreferredSize(new Dimension(42, 50));
-            title.setHorizontalTextPosition(SwingConstants.CENTER);
-            title.setHorizontalAlignment(SwingConstants.CENTER);
-            title.setBackground(new Color(204, 255, 255));
-            panel3.add(title, BorderLayout.NORTH);
-
-            //======== panel4 ========
+            //======== panel2 ========
             {
-                panel4.setBackground(new Color(102, 255, 204));
-                panel4.setDividerSize(2);
+                panel2.setLayout(new BorderLayout());
 
                 //======== panel1 ========
                 {
                     panel1.setPreferredSize(new Dimension(180, 40));
                     panel1.setBackground(new Color(102, 255, 204));
                     panel1.setBorder(new EmptyBorder(5, 5, 5, 5));
-                    panel1.setLayout(new GridLayout(9, 1, 10, 10));
+                    panel1.setLayout(new GridLayout(5, 1, 10, 10));
 
                     //---- newsfeedButton ----
                     newsfeedButton.setText("Newsfeed");
@@ -127,55 +129,73 @@ public class GuestHomeGUI extends JFrame {
                     newsfeedButton.addActionListener(e -> newsfeedButtonActionPerformed(e));
                     panel1.add(newsfeedButton);
 
-                    //---- profileSearchButton ----
-                    profileSearchButton.setText("Search Profiles");
-                    profileSearchButton.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-                    profileSearchButton.addActionListener(e -> profileSearchButtonActionPerformed(e));
-                    panel1.add(profileSearchButton);
-
-                    //---- topicSearchButton ----
-                    topicSearchButton.setText("Search Topics");
-                    topicSearchButton.setFont(new Font("Segoe UI", Font.PLAIN, 16));
-                    topicSearchButton.addActionListener(e -> topicSearchButtonActionPerformed(e));
-                    panel1.add(topicSearchButton);
-
                     //---- logoutButton ----
                     logoutButton.setText("Logout");
                     logoutButton.setFont(new Font("Segoe UI", Font.PLAIN, 16));
                     logoutButton.addActionListener(e -> logoutButtonActionPerformed(e));
                     panel1.add(logoutButton);
                 }
-                panel4.setLeftComponent(panel1);
+                panel2.add(panel1, BorderLayout.CENTER);
 
-                //======== scrollPane1 ========
+                //======== panel5 ========
                 {
+                    panel5.setBackground(new Color(102, 255, 204));
+                    panel5.setBorder(new EmptyBorder(5, 5, 5, 5));
+                    panel5.setLayout(new GridLayout(3, 1, 0, 8));
 
-                    //---- textArea1 ----
-                    textArea1.setFont(new Font("Monospaced", Font.PLAIN, 14));
-                    textArea1.setMargin(new Insets(15, 15, 15, 5));
-                    textArea1.setEditable(false);
-                    textArea1.setText("Welcome!");
-                    scrollPane1.setViewportView(textArea1);
+                    //---- textField1 ----
+                    textField1.setMargin(new Insets(5, 5, 5, 5));
+                    textField1.setFont(new Font("Segoe UI", Font.ITALIC, 16));
+                    textField1.setText("Enter search term here...");
+                    panel5.add(textField1);
+
+                    //---- profileSearchButton ----
+                    profileSearchButton.setText("Search Profiles");
+                    profileSearchButton.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+                    profileSearchButton.addActionListener(e -> profileSearchButtonActionPerformed(e));
+                    panel5.add(profileSearchButton);
+
+                    //---- topicSearchButton ----
+                    topicSearchButton.setText("Search Topics");
+                    topicSearchButton.setFont(new Font("Segoe UI", Font.PLAIN, 16));
+                    topicSearchButton.addActionListener(e -> topicSearchButtonActionPerformed(e));
+                    panel5.add(topicSearchButton);
                 }
-                panel4.setRightComponent(scrollPane1);
+                panel2.add(panel5, BorderLayout.PAGE_END);
             }
-            panel3.add(panel4, BorderLayout.CENTER);
+            panel4.add(panel2, BorderLayout.WEST);
+
+            //======== scrollPane1 ========
+            {
+                scrollPane1.setPreferredSize(new Dimension(0, 52));
+
+                //---- textArea1 ----
+                textArea1.setPreferredSize(new Dimension(50, 50));
+                textArea1.setFont(new Font("Monospaced", Font.PLAIN, 14));
+                textArea1.setMargin(new Insets(15, 15, 15, 5));
+                textArea1.setEditable(false);
+                textArea1.setText("Welcome!");
+                scrollPane1.setViewportView(textArea1);
+            }
+            panel4.add(scrollPane1, BorderLayout.CENTER);
         }
-        contentPane.add(panel3, BorderLayout.CENTER);
+        contentPane.add(panel4, BorderLayout.CENTER);
         setLocationRelativeTo(getOwner());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - Catherine Merz
-    private JPanel panel3;
     private JLabel title;
-    private JSplitPane panel4;
+    private JPanel panel4;
+    private JPanel panel2;
     private JPanel panel1;
     private JButton newsfeedButton;
+    private JButton logoutButton;
+    private JPanel panel5;
+    private JTextField textField1;
     private JButton profileSearchButton;
     private JButton topicSearchButton;
-    private JButton logoutButton;
     private JScrollPane scrollPane1;
     private JTextArea textArea1;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
